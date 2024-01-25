c0379f93680e177d1750934f97082909878973ce
viniciushiguchi <vinicius-higuchi@outlook.com>
2024-01-25 18:27:19 -0300

    terminado node do mapa

--- scripts/map/arrow.gd
+++ scripts/map/arrow.gd
@@ -4,12 +4,12 @@ extends TextureRect
 @onready var button = $"../Button"
 
 func _ready():
-	set_nodes(button, button_2)
+	pass
 
 func set_nodes(node1, node2):
 	var vector = node2.position - node1.position
 
 	position =  Vector2(node1.size.x/2, node1.size.y/2) + node1.position
-	size = Vector2(vector.length(),0)
+	size = Vector2(vector.length(),6)
 	rotation = vector.angle()
 	
