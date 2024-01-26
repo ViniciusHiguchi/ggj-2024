07302b93c258d63e7022a4918837e64ee6f69830
Cortesz <luccas.h.cortes@hotmail.com>
2024-01-25 23:48:30 -0300

    help main menu

--- scripts/menu_scripts/in_game_menu.gd
+++ scripts/menu_scripts/in_game_menu.gd
@@ -1,4 +1,5 @@
 extends CanvasLayer
+var is_help = false
 
 func _ready():
 	
@@ -21,11 +22,26 @@ func _on_mapa_pressed():
 	$HelpPage.hide()
 
 func _on_help_pressed():
-	$Jogadores_5.hide()
-	$Jogadores_6.hide()
-	$Jogadores_7.hide()
-	$Jogadores_8.hide()
-	$HelpPage.show()
+	if is_help:
+		$Jogadores_1.show()
+		$Jogadores_2.show()
+		$Jogadores_3.show()
+		$Jogadores_4.show()
+		$Mapa.show()
+		$Reset.show()
+		$HelpPage.hide()
+		is_help = false
+		
+	else:
+		$Jogadores_1.hide()
+		$Jogadores_2.hide()
+		$Jogadores_3.hide()
+		$Jogadores_4.hide()
+		$Mapa.hide()
+		$Reset.hide()
+		$HelpPage.show()
+		is_help = true
+		
 
 func _on_jogadores_1_pressed():
 	$Jogadores_5.show()
